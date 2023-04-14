b058684cdc40a9c437287f0aefcbc22a20e39686
palacioss2003 <palacioslagunamanuel@gmail.com>
2023-04-14 09:52:21 +0200

    Golderos feo

--- src/Entrnos_Tema_5/HolaGit.java
+++ src/Entrnos_Tema_5/HolaGit.java
@@ -5,7 +5,7 @@ public class HolaGit {
 	public static void main(String[] args) {
 		System.out.println("Hola Git");
 		System.out.println("Adios Git");
-		System.out.println("Actualizando...");
+		System.out.println("Actualiizando...");
 	}
 
 }
